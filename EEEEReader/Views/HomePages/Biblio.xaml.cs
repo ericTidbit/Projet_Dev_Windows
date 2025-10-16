46add87f19148ea364c57b9e1f093a2949271744
felix <6244794@cegepoutaouais.qc.ca>
2025-10-16 17:55:02 -0500

    choisir un fichier

--- EEEEReader/Views/HomePages/Biblio.xaml.cs
+++ EEEEReader/Views/HomePages/Biblio.xaml.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +6,16 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Windows.Storage.Pickers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,4 +31,23 @@ public sealed partial class Biblio : Page
     {
         InitializeComponent();
     }
+    public async void SelectionFichier(object sender, RoutedEventArgs e)
+    {
+        string? path = await choisirFichierUtilisateur(App.MainWindow!);
+
+    }
+    public async Task<string?> choisirFichierUtilisateur(Window window)
+    {
+        var hwnd = WindowNative.GetWindowHandle(window);
+        var winId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+        var appWin = AppWindow.GetFromWindowId(winId);
+
+        var picker = new FileOpenPicker(appWin.Id);
+        picker.FileTypeFilter.Add("*");
+
+        var file = await picker.PickSingleFileAsync();
+        return file?.Path;
+    }
+
+
 }
