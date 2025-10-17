92ee34e5c205928e5ffc6793f21cae3c2e6615bf
Julien-Turcotte <6315237@cegepoutaouais.qc.ca>
2025-10-16 23:18:30 -0400

    new class appli qui contient la liste de users

---
+++ EEEEReader/Models/Appli.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEEEReader.Models
+{
+    public class Appli
+    {
+        public List<Client> Clients { get; set; } = new List<Client>();
+        public List<Administrateur> Admins { get; set; } = new List<Administrateur>();
+
+        public Appli()
+        {
+            
+        }
+
+        public void AddClient(string nom, string pwd)
+        {
+            var client = new Client(nom, pwd);
+            Clients.Add(client);
+        }
+
+        public void AddAdmin(string nom, string pwd)
+        {
+            var admin = new Administrateur(nom, pwd);
+            Admins.Add(admin);
+        }
+
+        public bool CheckLogin(string nom, string pwd)
+        {
+            foreach (var client in Clients)
+            {
+                if (client.Nom == nom && client.Pwd == pwd)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
