95308c1150144c8dd910483e8a3e2d168bd87c02
ericTidbit <2467672@cegepoutaouais.qc.ca>
2025-10-19 14:32:28 -0400

    Replace Listview by Gridview and Make test function for byte[] to BitmapImage conversion

--- EEEEReader/Models/Livre.cs
+++ EEEEReader/Models/Livre.cs
@@ -1,10 +1,14 @@
 ﻿using EEEEReader.Models;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using VersOne.Epub.Options;
+using Windows.Storage.Streams;
 
 namespace EEEEReader.Models
 {
@@ -17,10 +21,11 @@ namespace EEEEReader.Models
         public string ISBN { get; set; }
         public string Langue { get; set; }
         public string Resume{ get; set; }
-        public byte[] cover { get; set; }
+        public byte[] CoverRaw { get; set; }
+        public BitmapImage? CoverImage { get; set; }
 
 
-        public Livre(string content, string Titre, string Auteur, string Date, string ISBN, string Langue, string Resume,byte[] cover)
+        public Livre(string content, string Titre, string Auteur, string Date, string ISBN, string Langue, string Resume, byte[] cover)
         {
             this.Content = content;
             this.Titre = Titre;
@@ -29,11 +34,42 @@ namespace EEEEReader.Models
             this.ISBN = ISBN;
             this.Langue = Langue;
             this.Resume = Resume;
-            this.cover = cover;
+            this.CoverRaw = cover;
+            this.LoadCoverImage(cover);
         }
+
 
         public Livre()
         {
+        }
+
+        // code de Andrei Ashikhmin, https://stackoverflow.com/questions/42523593/convert-byte-to-windows-ui-xaml-media-imaging-bitmapimage
+        // modifié
+        // soit cette méthode ne marche pas, ou EpubReader est cooked
+        public async void LoadCoverImage(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                this.CoverImage = null;
+            }
+
+            BitmapImage bitmapImage = new BitmapImage();
+
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                using (DataWriter writer = new DataWriter(stream))
+                {
+                    writer.WriteBytes(data);
+                    await writer.StoreAsync();
+                    await writer.FlushAsync();
+                    writer.DetachStream();
+                }
+
+                stream.Seek(0);
+                await bitmapImage.SetSourceAsync(stream);
+            }
+
+            this.CoverImage = bitmapImage;
         }
     }
 }
