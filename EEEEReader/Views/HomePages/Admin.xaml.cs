94537b6be79f3b826fda0c513a9ca22b95e7a30f
ericTidbit <2467672@cegepoutaouais.qc.ca>
2025-10-20 11:30:29 -0400

    Fix nav naming convention

--- EEEEReader/Views/HomePages/Admin.xaml.cs
+++ EEEEReader/Views/HomePages/Admin.xaml.cs
@@ -21,9 +21,9 @@ namespace EEEEReader.Views.HomePages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class Admin : Page
     {
-        public BlankPage1()
+        public Admin()
         {
             InitializeComponent();
         }
