0cf40cefa91eadae946f5f1fe0beb5161dee9cac
Julien-Turcotte <6315237@cegepoutaouais.qc.ca>
2025-11-06 11:52:27 -0500

    creation seeder et dbContext

---
+++ EEEEReader.Data/DataSeeder.cs
@@ -0,0 +1,44 @@
+﻿using EEEEReader.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEEEReader.Data
+{
+    public class DataSeeder
+    {
+        private readonly EEEEReaderDbContext _context;
+        public DataSeeder(EEEEReaderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            // Ne seed que si BD vide
+            if (_context.Livres.Any())
+                return;
+
+            List<Livre> Livres = new List<Livre>
+            {
+                new Livre(
+                    RawContent: null,
+                    Titre: "Livre exemple",
+                    Auteur: "Auteur exemple",
+                    Date: "2024",
+                    ISBN: "978-0000000000",
+                    Langue: "fr",
+                    Resume: "Résumé exemple",
+                    CoverRaw: null,
+                    CoverImage: null,
+                    HtmlContentList: null
+                )
+            };
+
+            _context.Livres.AddRange(Livres);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ EEEEReader.Data/EEEEReaderDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EEEEReader.Data.Models;
+
+namespace EEEEReader.Data
+{
+    public class EEEEReaderDbContext : DbContext
+    {
+        public EEEEReaderDbContext(DbContextOptions<EEEEReaderDbContext> options) : base(options) { }
+
+        public EEEEReaderDbContext() : base() { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Livre> Livres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string dbPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "EEEEreader", "EEEEReader.db");
+
+                Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+
+                optionsBuilder.UseSqlite($"Data Source={dbPath}");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
