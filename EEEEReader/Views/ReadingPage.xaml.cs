49b64e17c8bc84f65b5fd58f0f178eb1c1a33079
felix <6244794@cegepoutaouais.qc.ca>
2025-11-08 16:37:08 -0500

    refactor du style dans view completer

--- EEEEReader/Views/ReadingPage.xaml.cs
+++ EEEEReader/Views/ReadingPage.xaml.cs
@@ -1,5 +1,6 @@
 using EEEEReader.Models;
 using HtmlAgilityPack;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -19,6 +20,7 @@ using System.Text.RegularExpressions;
 using VersOne.Epub;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Text;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -120,7 +122,7 @@ public sealed partial class ReadingPage : Page
                 {
                     Paragraph para = new Paragraph();
 
-                    List<Run> styledRuns = Livre.ApplyStyle(node);
+                    List<Run> styledRuns = ApplyStyle(node);
 
                     foreach (Run styledRun in styledRuns)
                     {
@@ -138,7 +140,7 @@ public sealed partial class ReadingPage : Page
                 {
                     Paragraph para = new Paragraph();
 
-                    List<Run> styledRuns = Livre.ApplyStyle(node, new List<string>() { node.Name });
+                    List<Run> styledRuns = ApplyStyle(node, new List<string>() { node.Name });
 
                     foreach (Run styledRun in styledRuns)
                     {
@@ -161,7 +163,7 @@ public sealed partial class ReadingPage : Page
 
     public BitmapImage? GetImgFromSrc(Livre livre ,string src)
     {
-        Match match = Regex.Match(src, @"[^\/]+\.\w\S*");
+        Match match = Regex.Match(src, @"[^\\/]+\.\w\S*");
 
         if (match.Success)
         {
@@ -177,6 +179,196 @@ public sealed partial class ReadingPage : Page
         return null;
 
     }
+
+    // style
+    // partiellement généré par copilot
+    public static List<HtmlNode> FlattenHtmlDocument(HtmlDocument MainDocument)
+    {
+        List<HtmlNode> flatList = new List<HtmlNode>();
+        void Traverse(HtmlNode node)
+        {
+            flatList.Add(node);
+            if (node.HasChildNodes)
+            {
+                foreach (HtmlNode child in node.ChildNodes)
+                {
+                    Traverse(child);
+                }
+            }
+        }
+
+        foreach (HtmlNode parentNode in MainDocument.DocumentNode.ChildNodes)
+        {
+            Traverse(parentNode);
+        }
+
+        return flatList;
+    }
+    public static List<HtmlNode> FlattenHtmlNode(HtmlNode rootNode)
+    {
+        List<HtmlNode> flatList = new List<HtmlNode>();
+        void Traverse(HtmlNode node)
+        {
+            flatList.Add(node);
+            if (node.HasChildNodes)
+            {
+                foreach (HtmlNode child in node.ChildNodes)
+                {
+                    Traverse(child);
+                }
+            }
+        }
+
+        Traverse(rootNode);
+
+        return flatList;
+    }
+
+
+
+    public static List<Run> ApplyStyle(HtmlNode rootNode, List<string> startFlags = null)
+    {
+        List<Run> outputRuns = new List<Run>();
+
+        List<HtmlNode> flatRootNode = Livre.FlattenHtmlNode(rootNode);
+        List<string> nextStyleFlags = new List<string>();
+
+        if (startFlags != null)
+        {
+            nextStyleFlags.AddRange(startFlags);
+        }
+
+        foreach (HtmlNode node in flatRootNode)
+        {
+            (Run run, List<string> updatedStyleFlags) = ApplyStyleSwitch(node, nextStyleFlags);
+            nextStyleFlags = updatedStyleFlags;
+            if (run != null)
+            {
+                outputRuns.Add(run);
+            }
+        }
+
+        return outputRuns;
+
+    }
+
+
+    public static (Run, List<string>) ApplyStyleSwitch(HtmlNode node, List<string> styleFlags)
+    {
+        // TODO: traiter paragraphes avec des styles imbriqués (ex. <strong> du <em> italique </em> en gras </strong>)
+        switch (node.Name)
+        {
+            // n'enlève pas les styles, car est inline
+            case "span":
+            case "sup":
+                { return (null, styleFlags); }
+            // enlève les styles, pour éviter d'affecter les prochaines nodes
+            case "p":
+            case "div":
+                { return (null, new List<string>()); }
+            case "#text":
+                {
+
+                    Run run = new Run { Text = Livre.XmlPatternReplacer(node.InnerText) };
+                    if (styleFlags.Contains("em"))
+                    {
+                        run.FontStyle = Windows.UI.Text.FontStyle.Italic;
+                    }
+                    if (styleFlags.Contains("strong"))
+                    {
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    // ne peut pas avoir plusieurs headings en même temps
+                    if (styleFlags.Contains("h1"))
+                    {
+                        run.FontSize = 32;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    else if (styleFlags.Contains("h2"))
+                    {
+                        run.FontSize = 28;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    else if (styleFlags.Contains("h3"))
+                    {
+                        run.FontSize = 24;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    else if (styleFlags.Contains("h4"))
+                    {
+                        run.FontSize = 20;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    else if (styleFlags.Contains("h5"))
+                    {
+                        run.FontSize = 16;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+                    else if (styleFlags.Contains("h6"))
+                    {
+                        run.FontSize = 14;
+                        run.FontWeight = FontWeights.Bold;
+                    }
+
+                    // note: n'enlève pas les flags, les flags sont enlevés par les éléments en block (pas inline)
+                    return (run, styleFlags);
+                }
+            case "em":
+                {
+                    styleFlags.Add("em");
+                    return (null, styleFlags);
+                }
+            case "strong":
+                {
+                    styleFlags.Add("strong");
+                    return (null, styleFlags);
+                }
+            case "h1":
+            case "h2":
+            case "h3":
+            case "h4":
+            case "h5":
+            case "h6":
+                {
+                    styleFlags.Add(node.Name);
+                    return (null, styleFlags);
+                }
+            default:
+                {
+                    // enlève les styles, pour éviter d'affecter les prochaines nodes
+                    styleFlags.Clear();
+                    return (new Run { Text = "Unsupported node in ApplyStyle -- Node Type : " + node.Name }, styleFlags);
+                }
+
+        }
+    }
+
+    public static string XmlPatternReplacer(string input)
+    {
+        string output = input;
+        Dictionary<string, string> patternMap = new Dictionary<string, string>();
+
+        // TODO: plus de patterns
+        // https://www.ascii-code.com/
+        // format (pattern, remplacement)
+        patternMap.Add(@"&amp;", "&");
+        patternMap.Add(@"&#160;", "");
+        // ne marche pas rn
+        patternMap.Add(@"<\/?\w*>", "");
+
+        foreach (string pattern in patternMap.Keys)
+        {
+            output = Regex.Replace(output, pattern, patternMap[pattern]);
+        }
+
+        return output;
+    }
+
+
+    // style 
+
+
+
 
 
     // fin  content Panel Logic
