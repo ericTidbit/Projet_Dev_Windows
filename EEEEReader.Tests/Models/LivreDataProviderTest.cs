105ac81c8307074361bb65d3fc4c83a7e71c800a
ericTidbit <2467672@cegepoutaouais.qc.ca>
2025-12-04 11:35:46 -0500

    Add Felix test (Felix a codé les tests)

--- EEEEReader.Tests/Models/LivreDataProviderTest.cs
+++ EEEEReader.Tests/Models/LivreDataProviderTest.cs
@@ -4,6 +4,7 @@ using EEEEReader.Data.Models;
 using EEEEReader.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -111,6 +112,82 @@ namespace EEEEReader.Tests.Models
 
             // Assert
             Assert.AreEqual(42, updated.CurrentPage);
+        }
+
+
+        // Felix
+        [TestMethod]
+        public void AjouterPlusieursLivresPourUtilisateur_RetourneBonNombre()
+        {
+            Utilisateur u1 = new("u1", "pwd");
+            Utilisateur u2 = new("u2", "pwd");
+            _dataProvider.AjouterUtilisateur(u1);
+            _dataProvider.AjouterUtilisateur(u2);
+            var users = _dataProvider.GetUtilisateursData();
+
+            int id1 = users.First(u => u.Nom == "u1").Id;
+            int id2 = users.First(u => u.Nom == "u2").Id;
+
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "L1", Auteur = "A", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id1 });
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "L2", Auteur = "B", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id1 });
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "L3", Auteur = "C", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id2 });
+
+            Assert.AreEqual(2, _dataProvider.GetUtilisateurLivreData(id1).Count);
+            Assert.AreEqual(1, _dataProvider.GetUtilisateurLivreData(id2).Count);
+        }
+
+        [TestMethod]
+        public void ChangerPageDuLivre_IdInexistant_LanceInvalidOperationException()
+        {
+            int inexistId = 99999;
+            Assert.Throws<InvalidOperationException>(() => _dataProvider.ChangerPageDuLivre(inexistId, 10));
+        }
+
+        [TestMethod]
+        public void AjouterLivre_AssigneIdNonNul()
+        {
+            Utilisateur utilisateur = new("idtester", "pwd");
+            _dataProvider.AjouterUtilisateur(utilisateur);
+            int userId = _dataProvider.GetUtilisateursData().First().Id;
+
+            var livre = new Livre
+            {
+                Titre = "AvecId",
+                Auteur = "AuteurId",
+                FichierEpub = new byte[0],
+                CurrentPage = 0,
+                Pourcentage = 0,
+                UtilisateurId = userId
+            };
+            _dataProvider.AjouterLivreToUtilisateur(livre);
+
+            Livre livreFromDb = _dataProvider.GetUtilisateurLivreData(userId).First();
+            Assert.IsTrue(livreFromDb.Id > 0);
+        }
+
+        [TestMethod]
+        public void GetUtilisateurLivreData_IsoleLesLivresParUtilisateur()
+        {
+            Utilisateur u1 = new("iso1", "pwd");
+            Utilisateur u2 = new("iso2", "pwd");
+            _dataProvider.AjouterUtilisateur(u1);
+            _dataProvider.AjouterUtilisateur(u2);
+
+            var users = _dataProvider.GetUtilisateursData();
+            int id1 = users.First(u => u.Nom == "iso1").Id;
+            int id2 = users.First(u => u.Nom == "iso2").Id;
+
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "U1-L1", Auteur = "A", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id1 });
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "U2-L1", Auteur = "B", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id2 });
+            _dataProvider.AjouterLivreToUtilisateur(new Livre { Titre = "U1-L2", Auteur = "C", FichierEpub = new byte[0], CurrentPage = 0, Pourcentage = 0, UtilisateurId = id1 });
+
+            var livresU1 = _dataProvider.GetUtilisateurLivreData(id1);
+            var livresU2 = _dataProvider.GetUtilisateurLivreData(id2);
+
+            Assert.AreEqual(2, livresU1.Count);
+            Assert.AreEqual(1, livresU2.Count);
+            Assert.IsTrue(livresU1.All(l => l.UtilisateurId == id1));
+            Assert.IsTrue(livresU2.All(l => l.UtilisateurId == id2));
         }
     }
 }
