49cac72ec3e30b0b6ac00cc3d5fb8302cd553ebf
felix <6244794@cegepoutaouais.qc.ca>
2025-12-04 11:17:09 -0500

    commentaire de code

--- EEEEReader.Data/DataSeeder.cs
+++ EEEEReader.Data/DataSeeder.cs
@@ -1,6 +1,7 @@
 ﻿using EEEEReader.Data.Models;
 using EEEEReader.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System;
 using System.Linq;
 using VersOne.Epub; // Optionnel : uniquement si tu veux extraire la couverture
@@ -25,7 +26,7 @@ namespace EEEEReader.Data
             var admin = new Utilisateur("admin", "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea");
             _context.Utilisateurs.Add(admin);
             _context.SaveChanges(); 
-
+            // donc pour prendre le fichier epub j'ai mis directement dans une propriété de classe toute le binaire pour éviter le probleme de path
             byte[] fichierEpub = TestBooks.ConingsbyEpub;
 
             byte[] coverRaw = null;
