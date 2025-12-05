41412ffc836454a0a4cc73c963823291bc32b323
felix <6244794@cegepoutaouais.qc.ca>
2025-12-04 20:56:44 -0500

    les trois donné initial sont fait

--- EEEEReader.Data/DataSeeder.cs
+++ EEEEReader.Data/DataSeeder.cs
@@ -23,8 +23,12 @@ namespace EEEEReader.Data
             if (_context.Utilisateurs.Any() || _context.Livres.Any())
                 return;
 
-            var admin = new Utilisateur("admin", "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea");
+            var admin = new Utilisateur("admin", "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea"); // mot de passe : e 
+            var e = new Utilisateur("e", "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea");
+            var felix = new Utilisateur("felix", "3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea");
             _context.Utilisateurs.Add(admin);
+            _context.Utilisateurs.Add(e);
+            _context.Utilisateurs.Add(felix);
             _context.SaveChanges(); 
             // donc pour prendre le fichier epub j'ai mis directement dans une propriété de classe toute le binaire pour éviter le probleme de path
             byte[] fichierEpub = TestBooks.ConingsbyEpub;
@@ -41,6 +45,30 @@ namespace EEEEReader.Data
                 resume: "Roman politique de Benjamin Disraeli publié en 1844 sous le pseudonyme de « un jeune auteur ». Une œuvre clé du conservatisme britannique.",
                 coverRaw: coverRaw,
                 utilisateurId: admin.Id
+            );
+
+            var livreTest2 = new Livre(
+                fichierEpub: fichierEpub,
+                titre: "Coningsby",
+                auteur: "Benjamin Disraeli",
+                date: "1844",
+                isbn: null,
+                langue: "en",
+                resume: "Roman politique de Benjamin Disraeli publié en 1844 sous le pseudonyme de « un jeune auteur ». Une œuvre clé du conservatisme britannique.",
+                coverRaw: coverRaw,
+                utilisateurId: e.Id
+            );
+
+            var livreTest3 = new Livre(
+                fichierEpub: fichierEpub,
+                titre: "Coningsby",
+                auteur: "Benjamin Disraeli",
+                date: "1844",
+                isbn: null,
+                langue: "en",
+                resume: "Roman politique de Benjamin Disraeli publié en 1844 sous le pseudonyme de « un jeune auteur ». Une œuvre clé du conservatisme britannique.",
+                coverRaw: coverRaw,
+                utilisateurId: felix.Id
             );
 
             _context.Livres.Add(livreTest);
