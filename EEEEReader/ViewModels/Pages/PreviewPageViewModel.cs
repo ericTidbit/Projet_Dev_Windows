8650225ad4c58ee6adec4fd0c3135da34b35b236
6244794 <6244794@cegepoutaouais.qc.ca>
2025-12-06 13:18:26 -0500

    creation d'un viewmodel pour previewPage pour respecter mvvm

---
+++ EEEEReader/ViewModels/Pages/PreviewPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using EEEEReader.Data;
+using EEEEReader.Data.Models;
+using EEEEReader.Views.HomePages;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersOne.Epub;
+using WinRT.Interop;
+
+namespace EEEEReader.ViewModels.Pages
+{
+    public class PreviewPageViewModel : MainViewModel
+    {
+        public LivreViewModel PreviewedLivre { get; private set; }
+
+
+        private IDataProviderLivre _livreDataProvider;
+
+        public PreviewPageViewModel(IDataProviderLivre DataProvider, LivreViewModel previewedLivre)
+        {
+            _livreDataProvider = DataProvider;
+            PreviewedLivre = previewedLivre;
+        }
+        public void SuprimmerLivreClick()
+        { 
+           _livreDataProvider.SupprimerLivre(PreviewedLivre.Livre.Id);
+            
+        }
+
+
+
+
+    }
+}
